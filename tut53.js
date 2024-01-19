ec485f96e08d014f1e1b8cd877b123752745bd51
IAbdullahBinNasir <abdullahnasir2000@gmail.com>
2024-01-19 05:05:13 +0500

    First Commit

---
+++ tut53.js
@@ -0,0 +1,21 @@
+console.log("This is tutorial 53")
+
+function greet(name1, greetText="Greeting from JavaScript"){
+    let name = "Name1";
+    console.log(greetText + " " + name1);
+    console.log(name1 + " is a good boy");
+}
+let name1 = "Abdullah";
+let name2 = "Waqar";
+let name3 = "Aamir";
+let name4 = "Uzair";
+let greetText = "Good Morning"; 
+
+greet(name1, greetText);
+greet(name2, greetText);
+greet(name3, greetText);
+greet(name4);
+// console.log(name1 + " is a good boy");
+// console.log(name2 + " is a good boy");
+// console.log(name3 + " is a good boy");
+// console.log(name4 + " is a good boy");
